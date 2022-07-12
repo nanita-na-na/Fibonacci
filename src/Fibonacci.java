8c8cc62660413f69020ef086aaf8cb49cf763c33
Yanita <yanita2823@gmail.com>
2022-07-12 20:42:13 +0300

    Add a method for calculating the Fibonacci number

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,5 +1,15 @@
 public class Fibonacci {
     public static void main(String[] args) {
-        
+        System.out.println(fibonacciEffective(100)); // add your number
     }
+    private static int fibonacciEffective (int n) {
+        int [] arr = new int[n +1];
+
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i =2; i <= n; i++)
+            arr[i] = arr[i - 1] + arr[i - 2];
+            return arr[n];
+        }
 }
